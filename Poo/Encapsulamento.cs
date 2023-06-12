3765f75eca8ae2b496b0794c1043dca40f46c7c3
LUCAS HENRIQUE DA SILVA <lucas.hsilva50@senacsp.edu.br>
2023-06-12 14:13:20 -0300

    CursoCSharp

--- Poo/Encapsulamento.cs
+++ Poo/Encapsulamento.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Encapsulamento;
 
 namespace CursoCSharp.Poo {
+
+    public class FilhoNaoReconhecido : SubCelebridade {
+        public new void MeusAcessos() {
+            Console.WriteLine("Filho não reconhecido");
+            Console.WriteLine(InforPublica);
+            Console.WriteLine(CorDoOlho);
+        }
+    }
     class Encapsulamento {
         public static void Executar() {
+            SubCelebridade sub = new SubCelebridade();
+            sub.MeusAcessos();
+
+            new FilhoReconhecido().MeusAcessos();
+            new AmigoProximo().MeusAcessos();
+
+            new FilhoNaoReconhecido().MeusAcessos();
 
         }
     }
